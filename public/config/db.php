64d9dfab097243f680eab64ea653e05181b3fe10
belka176 <ssunset899@gmail.com>
2026-05-20 19:38:31 +0800

    скрыла конфигурационные файлы

--- public/config/db.php
+++ public/config/db.php
@@ -1,16 +1,25 @@
 <?php
 
-$host = "localhost";
-$dbname = "bookstore";
-$username = "root";
-$password = "";
+require 'env.php';
+
+loadEnv(__DIR__ . '/.env');
+
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch(PDOException $e) {
-    die("Ошибка подключения: " . $e->getMessage());
+
+    die("Ошибка подключения");
 }
-?>
